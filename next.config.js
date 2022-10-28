ac46727a2503fbf12783880a768465197aac8b4f
hieutranm2 <hieu.tm@mmh.co.jp>
2022-10-28 12:04:28 +0700

    Add Stylelint

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+const StylelintWebpackPlugin = require('stylelint-webpack-plugin')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['page.tsx'],
+  webpack(config) {
+    config.plugins.push(new StylelintWebpackPlugin())
+    return config
+  },
 }
 
 module.exports = nextConfig
